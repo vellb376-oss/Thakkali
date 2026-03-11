c5412f93110559d3020ed09b7909ec0764c6de13
vellb376-oss <vellb376@gmail.com>
2026-03-11 12:14:43 +0530

    Create m.py

---
+++ m.py
@@ -0,0 +1,8 @@
+# Program to multiply two numbers
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+result = num1 * num2
+
+print("Multiplication of", num1, "and", num2, "is", result)
